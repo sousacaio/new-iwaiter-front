a0a59d04c247e2b31f6406c35977027d33a2b04a
Shouganaii <frias.caio@yahoo.com.br>
2020-03-26 17:09:58 -0300

    Otimização de retornos no SignUp

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { Avatar, CssBaseline, TextField, FormControlLabel, Typography } from '@material-ui/core';
-import { Button, Checkbox, Link, Grid, Box } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Avatar, CssBaseline, TextField, Typography } from '@material-ui/core';
+import { Button, Link, Grid, Box } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Alert from '@material-ui/lab/Alert';
+import IconButton from '@material-ui/core/IconButton';
+import Collapse from '@material-ui/core/Collapse';
+import CloseIcon from '@material-ui/icons/Close';
+import api from '../../services/api';
 
 function Copyright() {
     return (
@@ -40,7 +45,36 @@ const useStyles = makeStyles(theme => ({
 
 export default function SignUp() {
     const classes = useStyles();
+    const [signup, setSignUp] = useState({ email: '', password: '', nome: '', endereco: '', phone: '' });
+    const [open, setOpen] = React.useState(false);
+    const [message, setMessage] = React.useState([]);
 
+    const handleSingUpChange = (e) => setSignUp({
+        ...signup, [e.target.name]: e.target.value,
+    });
+    async function handleSignUp(e) {
+        e.preventDefault();
+        const { email, password, nome, endereco, phone } = signup;
+
+        try {
+            const response = await api.post('/bar',
+                { email, password, nome, endereco, phone }
+            );
+            console.log(response)
+            const { data: { errors, message } } = response;
+            if (errors) {
+                setMessage(errors);
+                setOpen(true)
+            }
+            if (message) {
+                alert(message)
+            }
+
+        } catch (err) {
+            alert("Houve um problema com o cadastro, tente novamente mais tarde");
+        }
+
+    };
     return (
         <Container component="main" maxWidth="xs">
             <CssBaseline />
@@ -50,18 +84,19 @@ export default function SignUp() {
                 </Avatar>
                 <Typography component="h1" variant="h5">
                     Sign up
-        </Typography>
+                </Typography>
                 <form className={classes.form} noValidate>
                     <Grid container spacing={2}>
                         <Grid item xs={12} sm={6}>
                             <TextField
-                                autoComplete="fname"
-                                name="firstName"
                                 variant="outlined"
                                 required
                                 fullWidth
+                                value={signup.nome}
+                                name="nome"
+                                onChange={handleSingUpChange}
                                 id="firstName"
-                                label="First Name"
+                                label="Nome do bar"
                                 autoFocus
                             />
                         </Grid>
@@ -70,10 +105,23 @@ export default function SignUp() {
                                 variant="outlined"
                                 required
                                 fullWidth
-                                id="lastName"
-                                label="Last Name"
-                                name="lastName"
-                                autoComplete="lname"
+                                value={signup.phone}
+                                id="phone"
+                                onChange={handleSingUpChange}
+                                label="Telefone"
+                                name="phone"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                value={signup.endereco}
+                                id="phone"
+                                onChange={handleSingUpChange}
+                                label="Endereço"
+                                name="endereco"
                             />
                         </Grid>
                         <Grid item xs={12}>
@@ -82,8 +130,10 @@ export default function SignUp() {
                                 required
                                 fullWidth
                                 id="email"
+                                value={signup.email}
                                 label="Email Address"
                                 name="email"
+                                onChange={handleSingUpChange}
                                 autoComplete="email"
                             />
                         </Grid>
@@ -91,18 +141,14 @@ export default function SignUp() {
                             <TextField
                                 variant="outlined"
                                 required
+                                value={signup.password}
                                 fullWidth
+                                onChange={handleSingUpChange}
                                 name="password"
                                 label="Password"
                                 type="password"
                                 id="password"
                                 autoComplete="current-password"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                label="I want to receive inspiration, marketing promotions and updates via email."
                             />
                         </Grid>
                     </Grid>
@@ -111,15 +157,39 @@ export default function SignUp() {
                         fullWidth
                         variant="contained"
                         color="primary"
-                        className={classes.submit}
-                    >
+                        onClick={handleSignUp}
+                        className={classes.submit}>
+
                         Sign Up
-          </Button>
+                    </Button>
+                    <Collapse in={open}>
+                              {message.map((mensagens,i)=>{
+                                return <>   
+                                <Alert
+                            severity="error"
+                                action={
+                                    <IconButton
+                                        aria-label="close"
+                                        color="inherit"
+                                        size="small"
+                                        onClick={() => {setOpen(false);}}
+                                    >
+                                        <CloseIcon fontSize="inherit" />
+                                    </IconButton>
+                                }
+                            >                             
+                                {mensagens.message}
+                                 </Alert>
+                                </>
+                            })}
+                          
+                        </Collapse>
+                      
                     <Grid container justify="flex-end">
                         <Grid item>
                             <Link href="/" variant="body2">
                                 Já tenho uma conta!
-              </Link>
+                             </Link>
                         </Grid>
                     </Grid>
                 </form>
